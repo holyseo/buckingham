6238a994972dbab15f3b23ee1f40b8f23afec70d
holyseo <128103221+holyseo@users.noreply.github.com>
2024-02-26 14:52:15 +0000

    add responsiveness for iPad screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
         <div className=" relative bg-black">
           <img src="images/MaskGroup1.png" className="opacity-50" />
           <div className=" absolute inset-0 ">
-            <div className="flex flex-row justify-between items-start mt-[33px]  text-white text-[20px] px-14 font-Noto font-light">
-              <div className="flex flex-row justify-between w-[492px] py-4">
+            <div className="flex flex-row flex-wrap justify-center xl:justify-between gap-x-24 items-start mt-[10px] xl:mt-[33px] text-white text-[20px] px-14 font-Noto font-light">
+              <div className="flex flex-row justify-between w-[492px] py-3">
                 <div className=" cursor-pointer">Home</div>
                 <div className=" cursor-pointer">About</div>
                 <div className=" cursor-pointer">Our Artists</div>
@@ -21,32 +21,35 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="text-white mt-18 flex flex-col items-center">
-              <div className=" mt-36">
-                <img src="images/Image1.png" />
+            <div className="text-white mt-18 flex flex-col items-center justify-center">
+              <div className="mt-6 2xl:mt-20 ">
+                <img
+                  src="images/Image1.png"
+                  className="w-1/2 mx-auto lg:w-full"
+                />
               </div>
-              <div className="text-[40px] mt-12 font-Cormorant ">
+              <div className="text-[25px] xl:text-[40px] mt-5 xl:mt-12 font-Cormorant ">
                 INVESTING IN INNOVATIVE ARTISTS
                 <span className="text-[#AC9B65]"> SINCE 1983</span>
               </div>
-              <div className="w-[764px] mt-6 text-[20px] text-center tracking-wide font-Noto font-[300]">
+              <div className="w-[764px] mt-2 xl:mt-6 text-[12px] xl:text-[20px] text-center tracking-wide font-Noto font-[300] ">
                 Suspendisse at lorem id velit condimentum sagittis. Praesent ac
                 feugiat nisi. Class aptent taciti sociosqu ad litora torquent
                 per conubia nostra, per inceptos himenaeos. Suspendisse potenti.
                 Duis tincidunt ornare dignissim. Duis mi risus, tincidunt varius
                 condimentum in.
               </div>
-              <div className="mt-10 text-[20px] border-2 border-white px-4 py-2 cursor-pointer font-Noto font-[300]">
+              <div className="mt-3 xl:mt-10 text-[12px] xl:text-[20px] border-2 border-white px-4 py-2 cursor-pointer font-Noto font-[300]">
                 About us
               </div>
             </div>
           </div>
         </div>
         <div className="mt-20 flex flex-col items-center">
-          <div className="text-[#AC9B65] text-[50px] font-Cormorant">
+          <div className="text-[#AC9B65] text-[30px] xl:text-[50px] font-Cormorant">
             DUIS TINCIDUNT ORNARE DIGNISSIM
           </div>
-          <div className="mt-10 flex flex-row gap-8 items-center">
+          <div className="mt-10 flex flex-row flex-wrap gap-8 items-center justify-center">
             <div className="w-[534px] h-[616px] flex flex-col justify-between border-[1px] border-[#BEBEBE] shadow-[0px_0px_5px_rgba(0,0,0,0.25)] bg-white">
               <div className="w-fit">
                 <img src="images/Rectangle6.png" />
@@ -107,12 +110,12 @@ function App() {
           <div className="text-[#141414] text-[50px] font-Cormorant font-semibold">
             ARTIST SHOWCASE
           </div>
-          <div className="my-3 w-[883px] text-[#5F5F5F] text-[20px] font-Noto">
+          <div className="my-3 max-w-[883px] text-[#5F5F5F] text-[20px] font-Noto">
             Class aptent taciti sociosqu ad litora torquent per conubia nostra,
             per inceptos himenaeos. Suspendisse potenti. Duis tincidunt ornare
             dignissim.
           </div>
-          <div className="mt-8 flex flex-row justify-between items-center w-full px-32">
+          <div className="mt-8 flex flex-row flex-wrap justify-start 2xl:justify-between items-center w-full px-32 xl:gap-x-8">
             <div>
               <img
                 src="images/Rectangle15.png"
@@ -154,62 +157,62 @@ function App() {
             Browser Our Artists
           </div>
         </div>
-        <div className="relative bg-black font-Cormorant">
+        <div className="relative bg-black font-Cormorant ">
           <img src="images/Rectangle29.png" className="opacity-30" />
-          <div className="absolute inset-0  w-[1287px] flex flex-col gap-4 justify-center mx-auto px-10">
-            <div className="text-[54px] text-white  leading-tight">
+          <div className="absolute inset-0 max-w-[1287px] flex flex-col gap-4 justify-center mx-auto px-10">
+            <div className="text-[30px] xl:text-[54px] text-white leading-tight">
               Duis tincidunt ornare dignissim. Duis mi risus, tincidunt varius
               condimentum in. Suspendisse at lorem id velit condimentum
               sagittis. Praesent ac feugiat nisi.
             </div>
-            <div className="text-[32px] text-[#AC9B65]">
+            <div className="text-[20px] xl:text-[32px] text-[#AC9B65]">
               Duis tincidunt ornare dignissim.
             </div>
           </div>
         </div>
-        <div className="flex flex-row justify-between items-center mt-[116px] px-[116px]">
-          <div className="w-[825px] h-[377px] flex flex-row border-[1px] border-[#BEBEBE] shadow-[0px_0px_5px_rgba(0,0,0,0.25)] bg-white ">
+        <div className="flex flex-row flex-wrap justify-center 2xl:justify-between items-center mt-[116px] px-[116px] gap-y-12">
+          <div className="max-w-[825px] max-h-[377px] flex flex-row border-[1px] border-[#BEBEBE] shadow-[0px_0px_5px_rgba(0,0,0,0.25)] bg-white ">
             <div className="w-1/3">
               <img
                 src="images/Rectangle24.png"
-                className="w-[288px] h-[375px]"
+                className="xl:w-[288px] xl:h-[375px]"
               />
             </div>
-            <div className="w-2/3 flex flex-col justify-between p-[30px]">
-              <div className="text-[30px] font-Cormorant font-semibold">
+            <div className="w-2/3 flex flex-col justify-between p-[18px] xl:p-[30px] gap-5">
+              <div className="text-[28px] xl:text-[30px] font-Cormorant font-semibold">
                 SUSPENDISSE AT LOREM
               </div>
-              <div className="text-[20px] text-[#5F5F5F] font-Noto">
+              <div className="text-[18px] xl:text-[20px] text-[#5F5F5F] font-Noto">
                 Id velit condimentum sagittis. Praesent ac feugiat nisi. Class
                 aptent taciti sociosqu ad litora torquent per conubia nostra,
                 per inceptos himenaeos. Suspendisse potenti. Duis tincidunt
                 ornare dignissim. Duis mi risus, tincidunt varius condimentum
                 in.
               </div>
-              <div className=" bg-[#AC9B65] text-[20px] py-3 px-5 w-fit text-white cursor-pointer font-Noto ">
+              <div className=" bg-[#AC9B65] text-[16px] xl:text-[20px] py-3 px-5 w-fit text-white cursor-pointer font-Noto ">
                 Read more
               </div>
             </div>
           </div>
-          <div className="w-[825px] h-[377px] flex flex-row border-[1px] border-[#BEBEBE] shadow-[0px_0px_5px_rgba(0,0,0,0.25)] bg-white ">
+          <div className="max-w-[825px] max-h-[377px] flex flex-row border-[1px] border-[#BEBEBE] shadow-[0px_0px_5px_rgba(0,0,0,0.25)] bg-white ">
             <div className="w-1/3">
               <img
                 src="images/Rectangle26.png"
-                className="w-[288px] h-[375px]"
+                className="xl:w-[288px] xl:h-[375px]"
               />
             </div>
-            <div className="w-2/3 flex flex-col justify-between p-[30px]">
-              <div className="text-[30px] font-Cormorant font-semibold">
+            <div className="w-2/3 flex flex-col justify-between p-[18px] xl:p-[30px] gap-5">
+              <div className="text-[28px] xl:text-[30px] font-Cormorant font-semibold">
                 SUSPENDISSE POTENTI.
               </div>
-              <div className="text-[20px] text-[#5F5F5F] font-Noto">
+              <div className="text-[18px] xl:text-[20px] text-[#5F5F5F] font-Noto">
                 Praesent ac feugiat nisi. Class aptent taciti sociosqu ad litora
                 torquent per conubia nostra, per inceptos himenaeos. Duis
                 tincidunt ornare dignissim. Duis mi risus, tincidunt varius
                 condimentum in. Suspendisse at lorem id velit condimentum
                 sagittis.
               </div>
-              <div className=" bg-[#AC9B65] text-[20px] py-3 px-5 w-fit text-white cursor-pointer font-Noto ">
+              <div className=" bg-[#AC9B65] text-[16px] xl:text-[20px] py-3 px-5 w-fit text-white cursor-pointer font-Noto ">
                 Read more
               </div>
             </div>
@@ -224,7 +227,7 @@ function App() {
           <div>
             <img src="images/Image2.png" className="w-[271px] h-[98px]" />
           </div>
-          <div className="w-[1134px] flex flex-col justify-between items-center gap-3 text-[18px] text-[#5F5F5F] font-Noto">
+          <div className="max-w-[1134px] flex flex-col text-center justify-between items-center gap-3 text-[18px] text-[#5F5F5F] font-Noto">
             <p>
               © Buckingham Fine Art Publishers Ltd, Unit D, Denbigh West
               Business Park, Third Avenue, Milton Keynes, Bucks. MK1 1DH.
